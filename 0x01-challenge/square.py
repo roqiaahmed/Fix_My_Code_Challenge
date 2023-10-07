70b037de62be2b99becf2f9734e0e5c6de6e8fd9
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-07 16:40:56 +0300

    0x01-challenge/square.py

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class square():
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+        if self.height != self.width:
+            raise ValueError("Width and height must be equal for a square.")
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        return (self.width * 4)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
